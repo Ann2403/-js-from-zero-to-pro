2dc1ca8afe9dccacb659ebbf3d09fe7c383199ea
Ann <zubenko9529@gmail.com>
2020-08-11 18:11:37 +0300

    1 task

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 'use strict';
+let money = prompt("Ваш бюджет на месяц?", "10000");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "2020-01-01");
+
+let question = prompt("Введите обязательную статью расходов в этом месяцу");
+let answer = prompt("Во сколько обойдется?");
+
+let appDate = {
+    money: money,
+    timeData: time,
+    expenses: {
+        question: answer
+    },
+    optionalExpenses: {
+
+    },
+    income: [
+
+    ],
+    savings: false
+};
+
+alert("Ваш бюджет на 1 день: " + Math.trunc((money-answer)/30) + " грн");
