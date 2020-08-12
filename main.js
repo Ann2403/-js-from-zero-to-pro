294b4c34396b62c7eaaf502724de128125492a0b
Ann <zubenko9529@gmail.com>
2020-08-12 18:46:53 +0300

    two task

--- main.js
+++ main.js
@@ -1,23 +1,69 @@
 'use strict';
-let money = prompt("Ваш бюджет на месяц?", "10000");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "2020-01-01");
-
-let question = prompt("Введите обязательную статью расходов в этом месяцу");
-let answer = prompt("Во сколько обойдется?");
+let money = +prompt("Ваш бюджет на месяц?", "10000"),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "2020-01-01");
 
 let appDate = {
-    money: money,
+    budget: money,
     timeData: time,
-    expenses: {
-        question: answer
-    },
-    optionalExpenses: {
-
-    },
-    income: [
-
-    ],
+    expenses: { },
+    optionalExpenses: { },
+    income: [ ],
     savings: false
 };
 
-alert("Ваш бюджет на 1 день: " + Math.trunc((money-answer)/30) + " грн");
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяцу"),
+        b = prompt("Во сколько обойдется?");
+
+    if( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null &&
+        a != "" && b !="" && a.length < 50) {
+            console.log("done");
+            appDate.expenses[a] = b; 
+    } else {
+        i--;
+    }
+}
+
+/*
+let i = 0;
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяцу"),
+        b = prompt("Во сколько обойдется?");
+
+    if( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null &&
+        a != "" && b !="" && a.length < 50) {
+            console.log("done");
+            appDate.expenses[a] = b; 
+            i++;
+    } else {
+        i = 1;
+    }
+}
+
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяцу"),
+        b = prompt("Во сколько обойдется?");
+
+    if( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null &&
+        a != "" && b !="" && a.length < 50) {
+            console.log("done");
+            appDate.expenses[a] = b; 
+            i++;
+    } else {
+        i = 1;
+    }
+} while (i < 2);*/
+
+appDate.moneyPerDay = Math.trunc(appDate.budget/30);
+
+alert("Ваш бюджет на 1 день: " + appDate.moneyPerDay + " грн");
+
+if (appDate.moneyPerDay < 300) {
+    console.log("Минимальный уровень достатка!");
+} else if (appDate.moneyPerDay > 300 && appDate.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка!");
+} else if (appDate.moneyPerDay > 2000){
+    console.log("Высокий уровень достатка!");
+} else {
+    console.log("Произошла ошибка!");
+}
--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-/*I Love you*/
+let num = 50;
+
+/*if (num < 49) {
+    console.log("false");
+} else if (num > 100) {
+    console.log("num > 100");
+} else {
+    console.log("true");
+}
+
+(num < 49) ? console.log("false") : console.log("true");
+
+switch (num) {
+    case num < 49:
+        console.log("false");
+        break;
+    case num > 100:
+        console.log("num > 100");
+        break;
+    case num > 80:
+        console.log("num > 80");
+        break;
+    case 50:
+        console.log("true");
+        break;
+    default:
+        console.log("EROR");
+        break;
+} */
+
+/*while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+} while (num < 55); */
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
